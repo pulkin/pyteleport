5b56994ee5a262e7efd96fd259755eafcb72f740
Artem Pulkin <gpulkin@gmail.com>
2021-07-29 20:38:38 +0200

    0.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyteleport',
-    version='0.0',
+    version='0.1',
     author='pulkin',
     author_email='gpulkin@gmail.com',
     packages=find_packages(),
-    description='A proof-of-concept serialization, transmission and restoring python runtime',
+    description='A proof-of-concept snapshot, transmission and restoring python runtime',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[
