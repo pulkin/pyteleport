a29a490499ab5d510e84cfbb6ace7ab68fa3b7b1
Artem Pulkin <gpulkin@gmail.com>
2024-01-21 00:11:21 +0100

    tests: fix unstable tests

--- pyteleport/tests/test_frame.py
+++ pyteleport/tests/test_frame.py
@@ -7,12 +7,39 @@ def test_basic():
     frame = inspect.currentframe()
     wrapper = FrameWrapper(frame)
     assert wrapper.block_stack == []
-    assert wrapper.get_value_stack(2) == [wrapper.get_value_stack, 2]
+    assert wrapper.get_value_stack(0) == []
     locals_, cells, free = wrapper.get_locals_plus()
     for obj in frame, wrapper:
         assert obj in locals_
     assert cells == []
     assert free == []
+
+
+def test_value_stack():
+    frame = inspect.currentframe()
+    wrapper = FrameWrapper(frame)
+    for _ in range(1):
+        for __ in [2]:
+            range_iterator, list_iterator = wrapper.get_value_stack(2)
+            assert "range_iterator" in str(range_iterator)
+            assert "tuple_iterator" in str(list_iterator)
+
+
+def test_block_stack():
+    frame = inspect.currentframe()
+    wrapper = FrameWrapper(frame)
+
+    class DummyExc(Exception):
+        pass
+
+    try:
+        try:
+            block_stack = wrapper.block_stack
+        except DummyExc:
+            pass
+    finally:
+        pass
+    assert len(block_stack) == 2
 
 
 def test_generator_basic():
