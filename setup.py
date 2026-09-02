2b528652bd2d1296db7e194c80ef3fc15046c6d9
pulkin <gpulkin@gmail.com>
2026-09-02 21:28:59 +0100

    setup.py: remove duplicated info

--- setup.py
+++ setup.py
@@ -4,14 +4,7 @@ from Cython.Build import cythonize
 
 
 setup(
-    name='pyteleport',
-    version='0.2.1',
-    author='pulkin',
-    author_email='gpulkin@gmail.com',
     packages=find_packages(),
-    description='A proof-of-concept snapshot, transmission and restoring python runtime',
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
     ext_modules=cythonize([
         Extension("pyteleport.frame", ["cython/frame.pyx"]),
     ]),
